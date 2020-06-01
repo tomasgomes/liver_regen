618415333e5db6295e454469527a097893d1026b
Tomas PiresdeCarvalhoGomes <tpires@d.ethz.ch@bs-dsvr91.ethz.ch>
2020-06-01 10:47:11 +0200

    Integration of all cell datasets

--- aux_scripts/run_HarmonyIntegration_allcells.R
+++ aux_scripts/run_HarmonyIntegration_allcells.R
@@ -1,5 +1,5 @@
 # RUN SEURAT INTEGRATION ON ALL DATASETS
-# RUNTIME: 
+# RUNTIME: ~2h
 
 
 
@@ -10,15 +10,25 @@ library(harmony)
 # define working directory
 setwd("..") # project head directory
 
-# Load datasets
-load(file = "data/processed/healthy_srat_indivclust.RData")
-load(file = "data/processed/cond_srat_indivclust.RData")
-
-# Merge datasets (here by actually creating a single Seurat object)
-all_cell_l = c(healthy_srat, cond_srat)
-all_cell_srat = merge(all_cell_l[[1]], all_cell_l[[2]])
-for(n in names(all_cell_l)){
-  all_cell_srat = merge(all_cell_srat, all_cell_l[[n]])
+# Loading/creating the merged dataset
+merged_file = "data/processed/healthy_cond_merged_srat.RDS"
+if(!file.exists(merged_file)){
+  # Load datasets
+  load(file = "data/processed/healthy_srat_indivclust.RData")
+  load(file = "data/processed/cond_srat_indivclust.RData")
+  
+  # Merge datasets (here by actually creating a single Seurat object)
+  ## this is a long step
+  all_cell_l = c(healthy_srat, cond_srat)
+  all_cell_srat = merge(all_cell_l[[1]], all_cell_l[[2]])
+  for(n in names(all_cell_l)){
+    all_cell_srat = merge(all_cell_srat, all_cell_l[[n]])
+  }
+  
+  saveRDS(all_cell_srat, file = merged_file)
+} else{
+  # Load merged datasets
+  all_cell_srat = readRDS(merged_file)
 }
 
 # Re-run the Seurat normalisation for all cells (I don't think this is required)
@@ -26,8 +36,16 @@ for(n in names(all_cell_l)){
 #                            vars.to.regress = "nCount_RNA", variable.features.rv.th = 1,
 #                            return.only.var.genes = F, variable.features.n = NULL)
 
+
+# Run PCA on merged object
+all_cell_srat = RunPCA(all_cell_srat, features = rownames(all_cell_srat@assays$SCT@scale.data), 
+                       verbose = FALSE, assay = "SCT")
+
 # Run Harmony
-all_cell_srat = RunHarmony(all_cell_srat, "orig.ident", plot_convergence = F, assay.use = "SCT")
+all_cell_srat = RunHarmony(all_cell_srat, "orig.ident", tau = 30, plot_convergence = F, assay.use = "SCT")
+
+# Run UMAP on Harmony dimentions
+all_cell_srat = RunUMAP(all_cell_srat, reduction = "harmony", dims = 1:30)
 
 # save output
 saveRDS(all_cell_srat, file = "data/processed/scripts_out/HarmIntegr_allcells.RDS")
--- aux_scripts/run_SeuratIntegrationRPCA_allcells.R
+++ aux_scripts/run_SeuratIntegrationRPCA_allcells.R
@@ -1,5 +1,5 @@
-# RUN SEURAT INTEGRATION ON ALL DATASETS
-# RUNTIME: ~7h
+# RUN SEURAT INTEGRATION ON ALL DATASETS WITH rPCA
+# RUNTIME: ~4h
 
 
 
@@ -17,14 +17,21 @@ load(file = "data/processed/cond_srat_indivclust.RData")
 all_cell_srat = c(healthy_srat, cond_srat)
 
 # find features variable across all individual datasets
-integr_feat = SelectIntegrationFeatures(all_cell_srat, nfeatures = 1500)
+integr_feat = SelectIntegrationFeatures(all_cell_srat, nfeatures = 5000)
 
 # calculate possible missing Pearson residuals for SCTransform
 all_cell_srat = PrepSCTIntegration(all_cell_srat, anchor.features = integr_feat, verbose = T)
 
+all_cell_srat <- lapply(X = all_cell_srat, FUN = function(x) {
+  x <- ScaleData(x, features = integr_feat, verbose = FALSE)
+  x <- RunPCA(x, features = integr_feat, verbose = FALSE)
+})
+
 # finding the anchors for integration - in this case 30 dims should be a good default
 all_cell_anchors = FindIntegrationAnchors(all_cell_srat, normalization.method = "SCT", 
-                                          anchor.features = integr_feat, verbose = T)
+                                          anchor.features = integr_feat, reduction = "rpca",
+                                          assay = rep("SCT", length(all_cell_srat)), 
+                                          verbose = T)
 
 # actual data integration
 all_cell_integr = IntegrateData(all_cell_anchors, normalization.method = "SCT", verbose = T)
@@ -34,6 +41,4 @@ all_cell_integr = RunPCA(all_cell_integr, verbose = F)
 all_cell_integr = RunUMAP(all_cell_integr, dims = 1:30)
 
 # save output
-saveRDS(all_cell_integr, file = "data/processed/scripts_out/SeuratIntegr_allcells.RDS")
-
-
+saveRDS(all_cell_integr, file = "data/processed/scripts_out/SeuratIntegrRPCA_allcells.RDS")
